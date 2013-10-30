5c56d33a502f6b69412a84f8d178857573cd545d
allenhu <thefivefire@gmail.com>
2013-10-30 13:31:41 +0800

    format code

--- website/db.go
+++ website/db.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func getDB(username, userpwd, dbname string) (*sql.DB, error) {
-    dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8", username, userpwd, dbname)
-    db, err := sql.Open("mysql", dataSourceName)
-    if err != nil {
-        log.Println(err.Error())//仅仅是显示异常
-        return nil, err
-    }
-    return db, nil
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8", username, userpwd, dbname)
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Println(err.Error()) //仅仅是显示异常
+		return nil, err
+	}
+	return db, nil
 }
-
